c152bd2242e60d08628e1e3f0becf0911cc3fba7
DevMo-13 <moniquecinatl@gmail.com>
2021-02-27 17:58:51 -0800

    Added starter styling, and nav and footer components

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
 import React from 'react';
-import './App.scss';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
 
 function App() {
   	return (
-		<div className="App">
-			<header className="App-header">
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>Learn React
-				</a>
-			</header>
-		</div>
+		<>
+			<div className='content'>
+				<Nav />
+				<main>
+					<p>Let's eat tacos!</p>
+				</main>
+				<Footer />
+			</div>
+		</>
  	 );
 }
 
---
+++ src/components/Nav.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Nav() {
+	return (
+		<>
+			<nav>
+				<a href='/'>
+					<h1 className='title'>Into the Tacoverse</h1>
+				</a>
+			</nav>
+		</>
+	);
+}
+
+export default Nav;
