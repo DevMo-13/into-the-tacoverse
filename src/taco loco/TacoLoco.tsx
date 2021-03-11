90013abbae3f67251585f484abf933adf878eed0
DevMo-13 <moniquecinatl@gmail.com>
2021-03-10 16:40:37 -0800

    Condensed files and made code more dry

--- src/taco loco/TacoLoco.tsx
+++ src/taco loco/TacoLoco.tsx
@@ -1,8 +1,9 @@
-// Component to fetch data for a random taco recipe from TacoFancy API
+// Component to fetch data from the TacoFancy API and renders the random taco's info
 
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import TacoLocoDetails from './TacoLocoDetails';
+import { RiFileList3Line } from 'react-icons/ri';
+import TacoImg from './TacoImg';
 
 interface TacoResponse {
     base_layer: {
@@ -37,17 +38,18 @@ interface TacoResponse {
     };
 }
 
+const tacoLocoResponse: TacoResponse[] = [];
+
 function TacoLoco() {
-    // @ts-ignore
-    const [tacoData, setTacoData] = useState<TacoResponse>({});
+    const [tacoData, setTacoData] = useState(tacoLocoResponse);
 
     const randomTacoURL = 'http://taco-randomizer.herokuapp.com/random/';
     const getRandomTaco = () => {
         axios
-            .get(randomTacoURL)
+            .get<TacoResponse[]>(randomTacoURL)
             .then((response) => {
                 const taco = response.data;
-                console.log('rando taco: ', taco);
+                console.log('test rando taco: ', taco);
                 setTacoData(taco);
             })
             .catch((error) => {
@@ -60,9 +62,54 @@ function TacoLoco() {
     }, []);
 
     return (
-        <>
-            <TacoLocoDetails tacoData={tacoData} />
-        </>
+        <div className="tacoLoco">
+            <div className="intro">
+                <p>
+                    Feeling a little loco?
+                    <br></br>
+                    Try out a whole new twist on tacos by combining these unique
+                    recipes to make one
+                    <br></br>
+                    <br></br>
+                    <span id="loco">Taco Loco!</span>
+                </p>
+                <TacoImg />
+            </div>
+            <div className="ingredients">
+                <ul className="ingredientsUl">
+                    {Object.keys(tacoData).map((part: string) => (
+                        <>
+                            <li
+                                // @ts-ignore
+                                key={tacoData[part].name}
+                                className="ingredientLi"
+                            >
+                                <p
+                                    className="name"
+                                    // @ts-ignore
+                                    key={tacoData[part].name}
+                                >
+                                    {/*  @ts-ignore */}
+                                    {tacoData[part].name}
+                                </p>
+                                <a
+                                    // @ts-ignore
+                                    href={tacoData[part].url}
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    aria-label="recipe"
+                                    className="iconLink"
+                                    // @ts-ignore
+                                    key={tacoData[part].url}
+                                >
+                                    <RiFileList3Line className="recipeIcon" />
+                                </a>
+                            </li>
+                        </>
+                    ))}
+                </ul>
+            </div>
+        </div>
     );
 }
 
